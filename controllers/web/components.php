f32608c92051eadeb6f6c5901a8cf9f4160caab0
Tsifok <hermanmatias03@gmail.com>
2024-12-15 15:59:50 -0300

    creo archivos components.php

---
+++ controllers/web/components.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$page = "Componentes";
+$section = "components";
+require_once "../../views/layout.php";
+?>
--- views/home.php
+++ views/home.php
@@ -1,3 +1,4 @@
 <section class="ftco-section">
     <a href="register.php">Registrar empleado</a>
+    <a href="components.php">Componenetes</a>
 </section>
